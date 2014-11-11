35ee32b0fc9490abb658687213f5f9f43dea39a8
unknown <Ananya_Jana@WN7X64-52QJDS1.amer.dell.com>
2014-11-11 13:44:51 +0530

    fixing reg_id of school

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,6 +1,6 @@
 class School < ActiveRecord::Base
   attr_accessible :address, :district, :email_id, :fax, :latitude, :longitude, :phone, :phone2, :pin, :reg_id, :school_name, :state, :user_id
-  validates_format_of :reg_id, :with => /^[a-zA-Z][a-zA-Z\d_]*$/,:message => "can only be alphanumeric with no spaces"	
+  validates_format_of :reg_id, :with => /^[a-zA-Z\d_]*$/,:message => "can only be alphanumeric with no spaces"	
   validates_format_of :pin, :with => /(?:^|\D)\d{6}(?:\D|$)/,:message => "can only be 6 digit number with no spaces" 
   validates_format_of :phone, :with => /(?:^|\D)\d{10}(?:\D|$)/,:message => " can only be 10 digit mobile number with no spaces " 
   validates :phone2, :format => {:with => /(?:^|\D)\d{11}(?:\D|$)/}, :allow_nil => true
