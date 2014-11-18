4e1285e213aa9fc0ebc7419aac11a2fb00ca0c96
ananyajana02 <ananya_jana@dell.com>
2014-11-19 03:02:37 +0530

    removing the recaptcha key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lf2hP0SAAAAAFzUz43Z22xU24NBcCmBzBYZHB9I'
-  config.private_key = '6Lf2hP0SAAAAAJ1-RFSVPm1Q-91N57EjQdtI71Ix'
+  config.public_key  = ''
+  config.private_key = ''
 end
